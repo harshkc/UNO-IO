66fefd820cf5e668ae222fb510abb892a338081d
harshkc <harsh.hkc1006@gmail.com>
2021-12-14 23:39:43 +0530

    chore:adds homepage and setups routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,18 @@
-function App() {
-  return <div className='App'>APP</div>;
-}
+import "./App.css";
+import {Route, Routes} from "react-router-dom";
+import Homepage from "./components/Homepage";
+import Room from "./components/Room";
+
+const App = () => {
+  return (
+    <div className='App'>
+      <Routes>
+        <Route path='/' element={<Homepage />} />
+        <Route path='/play' element={<Homepage />} />
+        <Route path='/play/:roomid' element={<Room />} />
+      </Routes>
+    </div>
+  );
+};
 
 export default App;
---
+++ client/src/components/Homepage.js
@@ -0,0 +1,35 @@
+import React, {useState} from "react";
+import {Link} from "react-router-dom";
+import randomCodeGenerator from "../utils/randomCodeGenerator";
+
+const Homepage = () => {
+  const [roomCode, setRoomCode] = useState("");
+
+  return (
+    <div className='Homepage'>
+      <div className='homepage-menu'>
+        <img alt='uno-logo' src={require("../assets/logo.png").default} width='200px' />
+        <div className='homepage-form'>
+          <div className='homepage-join'>
+            <input
+              type='text'
+              placeholder='Game Code'
+              onChange={(event) => setRoomCode(event.target.value)}
+            />
+            <Link to={`/play/${roomCode}`}>
+              <button className='game-button green'>JOIN GAME</button>
+            </Link>
+          </div>
+          <h1>OR</h1>
+          <div className='homepage-create'>
+            <Link to={`/play/${randomCodeGenerator(5)}`}>
+              <button className='game-button orange'>CREATE GAME</button>
+            </Link>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Homepage;
