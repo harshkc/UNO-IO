b517dd41bc2aee6e770cebebacab0fd17f33f612
harshkc <harsh.hkc1006@gmail.com>
2021-12-21 23:25:32 +0530

    chore: re-setups routers

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,24 @@
 import "./App.css";
 import {Route, Routes} from "react-router-dom";
 import Homepage from "./components/Homepage";
+import {SoundProvider} from "./context/SoundProvider";
 import Room from "./components/Room";
+
+const GameWithSoundProvider = () => {
+  return (
+    <SoundProvider>
+      <Room />
+    </SoundProvider>
+  );
+};
 
 const App = () => {
   return (
     <div className='App'>
       <Routes>
         <Route path='/' element={<Homepage />} />
-        <Route path='/play' element={<Homepage />} />
-        <Route path='/play/:roomid' element={<Room />} />
+        <Route path='/play' exact element={<Homepage />} />
+        <Route path='/play/room=:roomid' element={<GameWithSoundProvider />} />
       </Routes>
     </div>
   );
